026ad9ab25caf116cdcd453ebeecfd7c8ec478cd
HalfLife <6436073+o0HalfLife0o@users.noreply.github.com>
2022-03-24 18:58:18 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -17,7 +17,7 @@ var Config = getMStr(function(){
 娱乐
 	哔哩哔哩, https://www.bilibili.com/, img/web/bilibili.ico
 	看看屋, https://www.kankanwu.com/, img/web/kankanwu.ico
-	奈菲影视, https://www.nfmovies.com/, img/web/nfmovies.ico
+	真不卡影院, http://www.zhenbuka3.com/, img/web/zhenbuka.ico
 	低端影视, https://ddrk.me/, img/web/ddrk.png
 	BL解析, https://vip.bljiex.com/, img/web/bljiex.ico
 资源
