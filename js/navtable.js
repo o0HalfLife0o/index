0f21fd32d5dc2bf8b284cb63d606d17ec41702d0
o0HalfLife0o <6436073+o0HalfLife0o@users.noreply.github.com>
2025-01-24 15:30:22 +0800

    update

--- js/navtable.js
+++ js/navtable.js
@@ -2,21 +2,21 @@ var navtableData = [
   {
     "type": "资讯",
     "sites": [
-      { "name": "IT 资讯", "url": "https://www.ithome.com/list/", "icon": "img/web/ithome.ico" },
-      { "name": "CNBeta", "url": "https://www.cnbeta.com.tw/", "icon": "img/web/cnBeta.ico" },
+      { "name": "NewsNow", "url": "https://newsnow.busiyi.world/", "icon": "img/web/newsnow.svg" },
+      { "name": "IT之家", "url": "https://www.ithome.com/list/", "icon": "img/web/ithome.ico" },
       { "name": "留园网", "url": "https://www.6park.com/usa.shtml", "icon": "img/web/6park.png" },
-      { "name": "澎湃新闻", "url": "https://www.thepaper.cn/", "icon": "img/web/thepaper.ico" },
-      { "name": "观察者", "url": "https://www.guancha.cn/", "icon": "img/web/guancha.ico" }
+      { "name": "卡饭", "url": "https://bbs.kafan.cn/forum-215-1.html", "icon": "img/web/kafan.ico" },
+      { "name": "Telegram", "url": "https://web.telegram.org/a/", "icon": "img/web/telegram.svg" }
     ]
   },
   {
-    "type": "社区",
+    "type": "综合",
     "sites": [
-      { "name": "Firefox吧", "url": "https://tieba.baidu.com/f?ie=utf-8&kw=firefox", "icon": "img/web/tieba.png" },
-      { "name": "卡饭", "url": "https://bbs.kafan.cn/forum-215-1.html", "icon": "img/web/kafan.ico" },
-      { "name": "果壳网", "url": "https://www.guokr.com/", "icon": "img/web/guokr.ico" },
-      { "name": "知乎网", "url": "https://www.zhihu.com/", "icon": "img/web/zhihu.ico" },
-      { "name": "Telegram", "url": "https://web.telegram.org/a/", "icon": "img/web/telegram.svg" }
+      { "name": "Google", "url": "https://www.google.com/ncr", "icon": "img/web/google.png" },
+      { "name": "淘宝", "url": "https://www.taobao.com/", "icon": "img/web/taobao.ico" },
+      { "name": "京东", "url": "https://www.jd.com/", "icon": "img/web/jd.ico" },
+      { "name": "NodeSeek", "url": "https://www.nodeseek.com/", "icon": "img/web/nodeseek.png" },
+      { "name": "新浪微博", "url": "https://weibo.com/", "icon": "img/web/weibo.ico" }
     ]
   },
   {
@@ -26,37 +26,27 @@ var navtableData = [
       { "name": "看看屋", "url": "https://www.kankanwu.com/", "icon": "img/web/kankanwu.ico" },
       { "name": "厂长影视", "url": "https://cz01.tv/", "icon": "img/web/czzy.png" },
       { "name": "低端影视", "url": "https://ddys.tv/", "icon": "img/web/ddrk.png" },
-      { "name": "BL解析", "url": "https://vip.bljiex.com/", "icon": "img/web/bljiex.ico" }
+      { "name": "YouTube", "url": "https://www.youtube.com/", "icon": "img/web/ytb.png" }
     ]
   },
   {
-    "type": "资源",
+    "type": "GPT",
     "sites": [
-      { "name": "腾讯视频", "url": "https://v.qq.com/", "icon": "img/web/qqvideo.ico" },
-      { "name": "爱奇艺", "url": "https://www.iqiyi.com/", "icon": "img/web/iqiyi.ico" },
-      { "name": "芒果TV", "url": "https://www.mgtv.com/", "icon": "img/web/mgtv.ico" },
-      { "name": "优酷", "url": "https://www.youku.com/", "icon": "img/web/youku.ico" },
-      { "name": "YouTube", "url": "https://www.youtube.com/", "icon": "img/web/ytb.png" }
+      { "name": "Gemini", "url": "https://gemini.google.com/", "icon": "img/web/gemini.svg" },
+      { "name": "Deepseek", "url": "https://chat.deepseek.com/", "icon": "img/web/deepseek.svg" },
+      { "name": "豆包", "url": "https://www.doubao.com/chat/", "icon": "img/web/doubao.png" },
+      { "name": "Copilot", "url": "https://github.com/copilot", "icon": "img/web/copilot.svg" },
+      { "name": "Kimi", "url": "https://kimi.moonshot.cn/", "icon": "img/web/kimi_ai.ico" }
     ]
   },
   {
     "type": "应用",
     "sites": [
-      { "name": "Dreamcast", "url": "http://dreamcast2.ys168.com/", "icon": "img/web/ys168.png" },
       { "name": "MEFCL", "url": "https://www.mefcl.com/", "icon": "img/web/mefcl.png" },
       { "name": "谷歌翻译", "url": "https://translate.google.com/", "icon": "img/web/translate.jpg" },
+      { "name": "网易云音乐", "url": "https://music.163.com/", "icon": "img/web/music.ico" },
       { "name": "百度地图", "url": "https://map.baidu.com/", "icon": "img/web/maps.ico" },
       { "name": "Github", "url": "https://github.com/", "icon": "img/web/github.svg" }
-    ]
-  },
-  {
-    "type": "综合",
-    "sites": [
-      { "name": "Google", "url": "https://www.google.com/ncr", "icon": "img/web/google.png" },
-      { "name": "淘宝", "url": "https://www.taobao.com/", "icon": "img/web/taobao.ico" },
-      { "name": "京东", "url": "https://www.jd.com/", "icon": "img/web/jd.ico" },
-      { "name": "网易云音乐", "url": "https://music.163.com/", "icon": "img/web/music.ico" },
-      { "name": "新浪微博", "url": "https://weibo.com/", "icon": "img/web/weibo.ico" }
     ]
   }
 ];
